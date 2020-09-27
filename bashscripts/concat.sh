7f26224a2e7d10fdf345bd325990fb5a32781ba5
Daniel Wegmann <daniel.wegmann@gmail.com>
2020-09-26 23:47:09 -0700

    clean getcsv

--- bashscripts/concat.sh
+++ bashscripts/concat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat ~/Documents/Project/bitinsight/csvs/address_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/address.csv
-cat ~/Documents/Project/bitinsight/csvs/tx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/tx.csv
-cat ~/Documents/Project/bitinsight/csvs/rel_adtx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_adtx.csv
-cat ~/Documents/Project/bitinsight/csvs/rel_txad_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_txad.csv
+cat ../csvs/address_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/address.csv
+cat ../csvs/tx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/tx.csv
+cat ../csvs/rel_adtx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_adtx.csv
+cat ../csvs/rel_txad_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_txad.csv
