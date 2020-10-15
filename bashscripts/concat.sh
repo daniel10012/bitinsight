0b5ef44df115f91f5fbd2f9333ba608ff231f3e2
Daniel Wegmann <daniel.wegmann@gmail.com>
2020-10-14 17:33:46 -0700

    modified bashscripts

--- bashscripts/concat.sh
+++ bashscripts/concat.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-cat ../csvs/address_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/address.csv
-cat ../csvs/tx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/tx.csv
-cat ../csvs/rel_adtx_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_adtx.csv
-cat ../csvs/rel_txad_df.csv/part* | sed -e '/^ *$/d' > clean_csvs/rel_txad.csv
-
 
 cat address_df/part* | sed -e '/^ *$/d' > address.csv
 cat tx_df/part* | sed -e '/^ *$/d' > tx.csv
@@ -15,17 +10,17 @@ cat rel_txad_df/part* | sed -e '/^ *$/d' > rel_txad.csv
 awk '{gsub(/\"/,"")};1' part-00000-0838e035-cb11-4678-a3ca-4db234b656a7-c000.csv > ad.csv
 sed 's/\\/ /g' ad.csv > ad2.csv
 
-uniq -w 26 address.csv > address2.csv
+# Spark Cluster to S3
+aws s3 cp address.csv s3://$bucket
+aws s3 cp tx.csv s3://$bucket
+aws s3 cp re_adtx.csv s3://$bucket
+aws s3 cp rel_txad.csv s3://$bucket
 
-Regex
+# Cluster to neo4j
 
-(?<![0-9])([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|1000)(?![0-9])     0 < 1000
-(?<![0-9])([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|10000)(?![0-9])
-http://gamon.webfactional.com/regexnumericrangegenerator/
-
-SELECT abs(hash(url)) % 10 as bucket, url, warc_filename, warc_record_offset, warc_record_length" +         " FROM ccindex WHERE subset = 'warc' AND content_languages='eng' " +         "AND (position('news' in url_host_name) != 0) df = spark.read.load(index_path)         df.createOrReplaceTempView("ccindex")          sqldf = spark.sql(args.query)
-
-
-key_list = [] for file in range(20200101, 20200132):     key_list.append(f'gdelt-data/{file}.export.CSV') gdelt_log = spark.read.csv(     s3_files, schema, sep='\t' )
+aws s3 cp s3://$bucket/address.csv localfolder
+aws s3 cp s3://$bucket/tx.csv  localfolder
+aws s3 cp s3://$bucket/re_adtx.csv localfolder
+aws s3 cp s3://$bucket/rel_txad.csv localfolder
 
 
