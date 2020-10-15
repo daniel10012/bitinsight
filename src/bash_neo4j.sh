a1ab3e8194ae7c7f0406490933ae2a5d10f286e5
Daniel Wegmann <daniel.wegmann@gmail.com>
2020-10-14 20:20:47 -0700

    final neo4j bash scripts

--- src/bash_neo4j.sh
+++ src/bash_neo4j.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # S3 to Neo4j
 
 aws s3 cp s3://$CONFIG.S3csvs/address_df [neo4j public DNS]/var/lib/imports
@@ -16,6 +17,9 @@ cat rel_adtx_df/part* | sed -e '/^ *$/d' > rel_adtx.csv
 cat rel_txad_df/part* | sed -e '/^ *$/d' > rel_txad.csv
 
 
+# Importing the blockchain
+
+sudo /usr/bin/neo4j-admin import --nodes=/var/lib/neo4j/import/tx_header.csv,/var/lib/neo4j/import/tx.csv --nodes=/var/lib/neo4j/import/address_header.csv,/var/lib/neo4j/import/address.csv --relationships=/var/lib/neo4j/import/rel_adtx_header.csv,/var/lib/neo4j/import/rel_adtx.csv --relationships=/var/lib/neo4j/import/rel_txad_header.csv,/var/lib/neo4j/import/rel_txad.csv --database=graph.db
 
 
 
